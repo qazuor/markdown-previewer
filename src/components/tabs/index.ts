1a1ae9d889eb216492c121b0f2caa21e264bde77
qazuor <qazuor@gmail.com>
2025-12-26 19:44:48 -0300

    feat(tabs): add editable tab name component
    - Double-click to edit document name inline
    - Validation with error display
    - Enter to save, Escape to cancel
    - Marks document as manually named

--- src/components/tabs/index.ts
+++ src/components/tabs/index.ts
@@ -1,4 +1,5 @@
-export { TabBar } from './TabBar';
-export { Tab } from './Tab';
 export { CloseConfirmModal } from './CloseConfirmModal';
+export { EditableTabName } from './EditableTabName';
+export { Tab } from './Tab';
+export { TabBar } from './TabBar';
 export { useTabs } from './hooks/useTabs';
